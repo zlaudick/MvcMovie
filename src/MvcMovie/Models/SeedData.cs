9615d783ec243f82a9e2e72cec7b60f773c383aa
Zac Laudick <zaclaudick@gmail.com>
2017-01-26 16:52:18 -0700

    added "Rating" field to database

--- src/MvcMovie/Models/SeedData.cs
+++ src/MvcMovie/Models/SeedData.cs
@@ -25,6 +25,7 @@ namespace MvcMovie.Models
                          Title = "When Harry Met Sally",
                          ReleaseDate = DateTime.Parse("1989-1-11"),
                          Genre = "Romantic Comedy",
+                         Rating = "PG-13",
                          Price = 7.99M
                      },
 
@@ -33,6 +34,7 @@ namespace MvcMovie.Models
                          Title = "Ghostbusters ",
                          ReleaseDate = DateTime.Parse("1984-3-13"),
                          Genre = "Comedy",
+                         Rating = "PG-13",
                          Price = 8.99M
                      },
 
@@ -41,6 +43,7 @@ namespace MvcMovie.Models
                          Title = "Ghostbusters 2",
                          ReleaseDate = DateTime.Parse("1986-2-23"),
                          Genre = "Comedy",
+                         Rating = "PG-13",
                          Price = 9.99M
                      },
 
@@ -49,6 +52,7 @@ namespace MvcMovie.Models
                        Title = "Rio Bravo",
                        ReleaseDate = DateTime.Parse("1959-4-15"),
                        Genre = "Western",
+                       Rating = "R",
                        Price = 3.99M
                    }
                 );
